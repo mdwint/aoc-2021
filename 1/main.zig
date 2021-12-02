65bcda93fc3e855e7584d5170a12c5c0fd77ec32
Matteo De Wint <matteo@gorilla.co>
2021-12-02 17:13:44 +0100

    Day 1; add Zig solution to part 1

---
+++ 1/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    var file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+    var buf: [1024]u8 = undefined;
+
+    var prev: u32 = std.math.maxInt(u32);
+    var num_increasing: usize = 0;
+
+    // Part 1:
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        var current = try std.fmt.parseInt(u32, line, 10);
+        if (current > prev) num_increasing += 1;
+        prev = current;
+    }
+
+    try stdout.print("{d}\n", .{num_increasing});
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent of Code 2021
 
 My solutions to [Advent of Code 2021](https://adventofcode.com/2021). I'll try
-to learn some Rust along the way.
+to learn some Rust along the way, and maybe even some Zig.
